8ae3727a54873a119cf46c79ad34269a51f9d40a
sovick-dev <sovickkonar32@gmail.com>
2023-04-16 21:22:34 +0530

    Update: cors middleware added

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const helmet  = require('helmet');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({
     extended : false
 }));
-app.use(helmet());
+
 
 module.exports = app;
